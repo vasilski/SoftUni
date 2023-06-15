654edef7b1bf1ae3c669ee9e022703a27b54d00e
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2023-06-15 14:14:16 +0300

    Add files via upload

---
+++ Fundamentals/E18TextProccessing/E04TextFilter.java
@@ -0,0 +1,14 @@
+package E18TextProccessing;
+
+import java.util.Scanner;
+
+public class E04TextFilter {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String keyWord = scan.nextLine();
+        String text = scan.nextLine();
+
+
+    }
+}
