579734fba83acbb3b9648b62dfbd266b62cf500e
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2023-06-15 15:11:55 +0300

    Update E04TextFilter.java

--- Fundamentals/E18TextProccessing/E04TextFilter.java
+++ Fundamentals/E18TextProccessing/E04TextFilter.java
@@ -1,14 +1,23 @@
-package E18TextProccessing;
-
 import java.util.Scanner;
 
 public class E04TextFilter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String keyWord = scan.nextLine();
+        String[] keyWord = scan.nextLine().split(", ");
         String text = scan.nextLine();
 
+        for (int i = 0; i < keyWord.length; i++) {
+            String replays = keyWord[i];
+            String wordOfStars = "";
+            for (int k = 0; k < replays.length(); k++) {
+                wordOfStars += "*";
+            }
 
+            text = text.replaceAll(keyWord[i], wordOfStars);
+
+        }
+
+        System.out.println(text);
     }
 }
