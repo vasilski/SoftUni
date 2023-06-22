d0b8203e58f5313819c418baa5f9c2e76fd4d3d8
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2023-06-22 10:52:43 +0300

    Add files via upload

---
+++ Java Advance/A04StacksAndQueuesExercises/E08InfixToPostfix.java
@@ -0,0 +1,41 @@
+package A04StacksAndQueuesExercises;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class E08InfixToPostfix {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Deque<String> stack = new ArrayDeque<>();
+        String text = "";
+
+        for (int i = 0; i < n; i++) {
+            String[] token = scanner.nextLine().split(" ");
+
+            switch (token[0]) {
+                case "1":
+                    stack.push(text);
+                    text += token[1];
+                    break;
+                case "2":
+                    stack.push(text);
+                    int count = Integer.parseInt(token[1]);
+                    text = text.substring(0, text.length() - count);
+                    break;
+                case "3":
+                    int index = Integer.parseInt(token[1]);
+                    System.out.println(text.charAt(index - 1));
+                    break;
+                case "4":
+                    text = stack.pop();
+                    break;
+            }
+        }
+    }
+
+}
