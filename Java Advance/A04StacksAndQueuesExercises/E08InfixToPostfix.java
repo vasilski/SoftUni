36cb9b774b26a8ae8bdebe03ab1bc3a6396dcb60
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2023-07-03 11:25:28 +0300

    Update E08InfixToPostfix.java

--- Java Advance/A04StacksAndQueuesExercises/E08InfixToPostfix.java
+++ Java Advance/A04StacksAndQueuesExercises/E08InfixToPostfix.java
@@ -1,41 +1,55 @@
 package A04StacksAndQueuesExercises;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+import java.util.*;
 
 public class E08InfixToPostfix {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        String[] input = scan.nextLine().split(" ");
+        ArrayDeque<String> operators = new ArrayDeque<>();
+        ArrayDeque<String> expression = new ArrayDeque<>();
+        Map<String,Integer> priorities = new HashMap<>();
+        priorities.put("*", 3);
+        priorities.put("/", 3);
+        priorities.put("+", 2);
+        priorities.put("-", 2);
+        priorities.put("(", 1);
 
-        int n = Integer.parseInt(scanner.nextLine());
-
-        Deque<String> stack = new ArrayDeque<>();
-        String text = "";
-
-        for (int i = 0; i < n; i++) {
-            String[] token = scanner.nextLine().split(" ");
-
-            switch (token[0]) {
-                case "1":
-                    stack.push(text);
-                    text += token[1];
-                    break;
-                case "2":
-                    stack.push(text);
-                    int count = Integer.parseInt(token[1]);
-                    text = text.substring(0, text.length() - count);
-                    break;
-                case "3":
-                    int index = Integer.parseInt(token[1]);
-                    System.out.println(text.charAt(index - 1));
-                    break;
-                case "4":
-                    text = stack.pop();
-                    break;
+        for (String anInput : input) {
+            try {
+                double num = Double.parseDouble(anInput);
+                expression.addLast(anInput);
+            } catch (Exception e) {
+                switch (anInput) {
+                    case "x":
+                        expression.addLast(anInput);
+                        break;
+                    case "(":
+                        operators.push(anInput);
+                        break;
+                    case ")":
+                        String symbol = operators.pop();
+                        while (!symbol.equals("(")) {
+                            expression.addLast(symbol);
+                            symbol = operators.pop();
+                        }
+                        break;
+                    default:
+                        while (!operators.isEmpty() && priorities.get(operators.peek()) >= priorities.get(anInput)) {
+                            expression.addLast(operators.pop());
+                        }
+                        operators.push(anInput);
+                        break;
+                }
             }
         }
-    }
+        while (!operators.isEmpty()) {
+            expression.addLast(operators.pop());
+        }
 
+        while (expression.size() > 0) {
+            System.out.print(expression.pop()+" ");
+        }
+    }
 }
