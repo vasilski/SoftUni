f99871134c7c0a07af3bd211d47356cafdddd26f
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-01-15 06:09:45 +0200

    Add files via upload

---
+++ Java Advance/A17Generics/E01JarOfT/Main.java
@@ -0,0 +1,12 @@
+package A17Generics.E01JarOfT;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Jar<Integer> jarNum = new Jar<>();
+
+        jarNum.add(13);
+
+        Jar<String> stringJar = new Jar<>();
+    }
+}
