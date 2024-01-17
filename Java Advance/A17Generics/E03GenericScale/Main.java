3157ffcce6f7746f11d43864cd80cafdbc936fbf
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-01-17 06:13:32 +0200

    Add files via upload

---
+++ Java Advance/A17Generics/E03GenericScale/Main.java
@@ -0,0 +1,8 @@
+package A17Generics.E03GenericScale;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ Java Advance/A17Generics/E03GenericScale/Scale.java
@@ -0,0 +1,22 @@
+package A17Generics.E03GenericScale;
+
+public class Scale<T extends Comparable<T>> {
+    private T left;
+    private T right;
+
+    public Scale(T left, T right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getHeavier() {
+        if (left == right) {
+            return null;
+        }
+        if (left.compareTo(right) > 0) {
+            return left;
+        }
+
+        return right;
+    }
+}
