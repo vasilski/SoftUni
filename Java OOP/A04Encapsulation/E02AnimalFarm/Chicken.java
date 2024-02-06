5354102e665b9b145362114ee6c537ae9ed27e73
Vasko Hristov <35739466+vasilski@users.noreply.github.com>
2024-02-06 06:15:40 +0200

    Add files via upload

---
+++ Java OOP/A04Encapsulation/E02AnimalFarm/Chicken.java
@@ -0,0 +1,45 @@
+package A04Encapsulation.E02AnimalFarm;
+
+public class Chicken {
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age < 0 || age > 15) {
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+
+    }
+
+    public double productPerDay() {
+        return this.calculateProductPerDay();
+    }
+
+    private double calculateProductPerDay() {
+        if (this.age < 6) {
+            return 2;
+        } else if (this.age < 12) {
+            return 1;
+        } else {
+            return 0.75;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("A04Encapsulation.E02AnimalFarm.Chicken %s (age %d) can produce %.2f eggs per day.", this.name, this.age, productPerDay());
+    }
+}
---
+++ Java OOP/A04Encapsulation/E02AnimalFarm/Main.java
@@ -0,0 +1,7 @@
+package A04Encapsulation.E02AnimalFarm;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
